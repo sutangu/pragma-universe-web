cc1c1518711c73edf28d5cd68f01ce91a04538fd
sutangu <sutangu@gmail.com>
2024-05-17 02:02:54 +0300

    #1 add event handlers to MobileMenu

--- src/components/header/MobileMenu/MobileMenu.tsx
+++ src/components/header/MobileMenu/MobileMenu.tsx
@@ -1,12 +1,61 @@
+'use client';
+
 import cs from './MobileMenu.module.scss';
 import { NavigationMenuInterface } from '@/interfaces';
 import Link from 'next/link';
+import { KeyboardEvent, useEffect, useRef } from 'react';
 
 const MobileMenu = ({ nav }: {nav: NavigationMenuInterface[]}) => {
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMenuItemClick = () => {
+    const checkbox = document.getElementById('menu__toggle') as HTMLInputElement | null;
+    if (checkbox && checkbox.type === 'checkbox') {
+      checkbox.checked = false;
+    }
+  };
+
+  const handleMenuItemKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      const checkbox = document.getElementById('menu__toggle') as HTMLInputElement | null;
+      if (checkbox && checkbox.type === 'checkbox') {
+        checkbox.checked = !checkbox.checked;
+      }
+    }
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      const checkbox = document.getElementById('menu__toggle') as HTMLInputElement | null;
+      if (checkbox && checkbox.type === 'checkbox') {
+        checkbox.checked = false;
+      }
+    }
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      const checkbox = document.getElementById('menu__toggle') as HTMLInputElement | null;
+      if (checkbox && checkbox.type === 'checkbox') {
+        checkbox.checked = false;
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown as EventListener);
+    document.addEventListener('mousedown', handleClickOutside as EventListener);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown as EventListener);
+      document.removeEventListener('mousedown', handleClickOutside as EventListener);
+    };
+  }, []);
+
+
   return (
-    <div className={cs.hamburger}>
+    <div ref={menuRef} className={cs.hamburger}>
       <input className={cs.hamburger__toggle} id="menu__toggle" type="checkbox" />
-      <label className={cs.hamburger__btn} htmlFor="menu__toggle">
+      <label onKeyDown={handleMenuItemKeyDown} tabIndex={0} role="button" className={cs.hamburger__btn} htmlFor="menu__toggle">
         <span></span>
       </label>
 
@@ -14,7 +63,7 @@ const MobileMenu = ({ nav }: {nav: NavigationMenuInterface[]}) => {
         {
           nav.map((item) => (
             <li className={cs.hamburger__item} key={item.path}>
-              <Link href={item.path}>
+              <Link onClick={handleMenuItemClick} href={item.path}>
                 {item.name}
               </Link>
             </li>
